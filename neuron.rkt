138abf7193ae92b277bc7ed6ad0fdecf32c30354
Skopjuk <ksenia.agag@gmail.com>
2017-04-23 17:07:46 +0300

    Update neuron.rkt

--- neuron.rkt
+++ neuron.rkt
@@ -1,5 +1,5 @@
 #lang racket
-
+#lang lazy
 (require racket/trace)
 (require math/matrix)
 
@@ -21,14 +21,13 @@
 (define syn0 (build-matrix 3 1 (lambda (i j) (- (* 2 (random)) 1))))
 
 (define (learn X y)
-  (for ([iter 100000])
+  (for ([iter 0])
     (define l0 X)
-    (define l1 (matrix-map nonlin (matrix-dot l0 syn0)))
+    (define l1 (matrix-map nonlin (matrix* l0 syn0)))
     (define l1_error (matrix- y l1))
-    (define l1_delta (matrix* l1_error matrix-map nonlin-deriv l1))
+    (define l1_delta (matrix-map * l1_error (matrix-map nonlin-deriv l1)))
     (print l1_delta)
-    (set! syn0 (matrix+ syn0 (matrix-dot (matrix-transpose l0) l1_delta)))
+    (set! syn0 (matrix+ syn0 (matrix* (matrix-transpose l0) l1_delta)))
     l1))
 
-(trace learn)
 (learn X y)
